5ae4ce8b04e41ccb44edeb1106e579fa6ea8a9ae
omidshahinmehr-coder <omidshahinmehr@gmail.com>
2026-09-13 05:15:51 +0330

    Update TafsirScreen.kt

--- app/src/main/java/com/lbo/quran/ui/TafsirScreen.kt
+++ app/src/main/java/com/lbo/quran/ui/TafsirScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.CompositionLocalProvider
+//import androidx.compose.ui.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
